53f219c0e0ae0de447575b9064475222273b9624
mariadeepthi7 <mariadeepthi7@gmail.com>
2024-09-12 18:06:52 +0530

    task1

---
+++ src/iteration/org/Iteratorsample.java
@@ -0,0 +1,25 @@
+package iteration.org;
+import java.util.Vector;
+public class Iteratorsample {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector<String>v =new Vector();
+		v.add("The");
+		v.add("world");
+		v.add("is");
+		v.add("beautiful");
+		v.add("Enjoy every movement");
+		java.util.Iterator<String> i = v.iterator();
+		while (i.hasNext()) {
+			String n =  i.next();
+			System.out.println(n);
+			if (n.equals("world")) {
+				i.remove();
+				
+			}
+			
+		}
+		System.out.println(v);
+	}
+
+}
